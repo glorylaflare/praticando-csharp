6c30e40cf5c87011d83544401ea538af5b2b9bbb
Marcelo Gonçalves <juninho_toh@hotmail.com>
2025-07-03 22:56:27 -0300

    first commit

---
+++ WebApplication1/WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+public class LoginController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    public IActionResult Signup()
+    {
+        return View();
+    }
+}
